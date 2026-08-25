0b1985046e5165327220e5679f8dc77102e4b6c7
root <root@hiplet-109548.tail6a4baa.ts.net>
2026-08-25 18:42:26 +0000

    miniapp: weather backgrounds v10 (city-right style, all 20)

--- miniapp/static/vendor/app.compiled.js
+++ miniapp/static/vendor/app.compiled.js
@@ -1040,7 +1040,7 @@ function App() {
           'clear-night': 'clear-night',
           'partly-cloudy-night': 'partly-cloudy-night'
         };
-        return (map[weather.icon] || 'partly-cloudy-background') + '.png?v=9';
+        return (map[weather.icon] || 'partly-cloudy-background') + '.png?v=10';
       })()}'`,
       backgroundSize: 'cover',
       backgroundPosition: 'right center'
